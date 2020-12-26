fdd16bb43adfc63e57f45e648af1fefed1d08ca5
zsxfa <1844577897@qq.com>
2020-12-26 20:13:52 +0800

    主干添加

--- HelloGit/src/main/java/com/jachin/git/HelloGit.java
+++ HelloGit/src/main/java/com/jachin/git/HelloGit.java
@@ -1,13 +1,10 @@
 package com.jachin.git;
 
-/**
- * @author zsxfa
- * @create 2020-12-26 19:22
- */
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("更新1");
         System.out.println("分支添加");
+        System.out.println("主干添加");
     }
 }
