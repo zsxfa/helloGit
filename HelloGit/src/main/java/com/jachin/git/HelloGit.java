0305a8b7756dac06eefa6df386d1009e290af8c2
zsxfa <1844577897@qq.com>
2020-12-26 19:51:16 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/jachin/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.jachin.git;
+
+/**
+ * @author zsxfa
+ * @create 2020-12-26 19:22
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
